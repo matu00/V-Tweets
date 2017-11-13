3c67a1aee24a26354356158e236ca6a1dacb149a
Matias <matti1020009@gmail.com>
2017-11-13 11:04:16 -0300

    new changes

--- index.php
+++ index.php
@@ -24,8 +24,12 @@
 
     <div id="particles-js">
       <div id="intro">
-        <?php include("views/includes/nav-bar.html"); ?>
-        <?php include("views/OtherViews/ChangeTextColor.html"); ?>
+        <?php
+          include("views/includes/nav-bar.html");
+          include("views/OtherViews/ChangeTextColor.html");
+          include("views/OtherViews/config.html"); 
+        ?>
+
         <div align="center" class="tittle container"><h1 class="text" style="font-family: 'Crimson Text', serif;" id="phrases">Across the Universe</h1></div>
         <h3 class="text" style="color: #dedede;font-family: 'Crimson Text', serif;" id="author">The Beatles</h3>
         <img class="img-circle" id="circleimg">
